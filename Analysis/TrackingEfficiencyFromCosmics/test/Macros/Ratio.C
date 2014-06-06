0b6496884402aa0bbf2893d2d9f64d4be5cceb26
Marco <marco.de.mattia@cern.ch>
2014-06-06 10:35:59 -0500

    Removed files

--- Analysis/TrackingEfficiencyFromCosmics/test/Macros/Ratio.C
+++
@@ -1,9 +0,0 @@
-{
-	TFile *f1 = new TFile("eff_MC_dz10.root");
-	TFile *f2 = new TFile("eff_data_dz10_new.root");
-	TH1F *h1 = f1->Get("effVsD0");
-	TH1F *h2 = f2->Get("effVsD0");
-	h2->Divide(h1);
-	h2->Draw();
-	h2
-}
